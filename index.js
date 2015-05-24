faed64823a142b80a0ef5eaea24dd872fa09ade6
Glenn Sonna <kenpachi.os@gmail.com>
2015-05-24 19:41:36 +0200

    photos

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ docReady( function() {
     if(!addVideo){
       var fileName = "zo_" + i;
 
-      $itemContent.style.background = 'url(photos/' + fileName + '.jpg)  50% 50%';
+      $itemContent.style.background = 'url(https://console.developers.google.com/m/cloudstorage/b/ladusoleil/o/photos/' + fileName + '.jpg)  50% 50%';
       $itemContent.style.backgroundSize = 'cover';
       $itemContent.style.backgroundRepeat = 'no-repeat';
     }else{
@@ -61,7 +61,7 @@ docReady( function() {
       var fileName = videoFile.name;
 
       var $video = document.createElement('video');
-      $video.src = 'videos/' + fileName;
+      $video.src = 'https://console.developers.google.com/m/cloudstorage/b/ladusoleil/o/' + fileName;
       $video.autoPlay = false;
       $video.innerHTML = 'Votre navigateur ne gère pas l\'élément <code>video</code>.'
       $video.setAttribute("controls","controls")  
